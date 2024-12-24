45971d7dd53a7bd51426a459c783c485ca921b35
quiesscent <ephesianslewis@gmail.com>
2024-12-24 20:43:16 +0300

    Requirements Update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware', 
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',list
 ]
 
 REST_FRAMEWORK = {
