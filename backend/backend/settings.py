0009e7226bf7251eb0657e08fea0803f2ebdedf9
quiesscent <ephesianslewis@gmail.com>
2024-12-24 20:51:50 +0300

    corrections

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware', 
     'corsheaders.middleware.CorsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',list
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 REST_FRAMEWORK = {
