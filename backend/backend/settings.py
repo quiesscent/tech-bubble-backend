20164ecd351168c3fcade7c17f9d86f3d2acffa7
quiesscent <ephesianslewis@gmail.com>
2025-01-27 20:31:58 +0300

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,10 +83,7 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:5173",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 ROOT_URLCONF = 'backend.urls'
