faba1fb8e4cd35573151e9def50f89245b076842
khalifa-pendrops <dikee5200@gmail.com>
2025-01-28 13:09:08 +0100

    first feature-blog commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Login from "./Login";
-import Signup from "./Signup";
-import ForgotPassword from "./ForgotPassword";
-import BlogPage from "./Components/BlogPage";
+import CreateBlogPost from "./Components/CreateBlogPost";
+// import Login from "./Login";
+// import Signup from "./Signup";
+// import ForgotPassword from "./ForgotPassword";
+// import BlogPage from "./Components/BlogPage";
 
 export default function App() {
   return (
@@ -14,7 +15,8 @@ export default function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/blog-page" element={<BlogPage />} />
       </Routes> */}
-      <BlogPage />
+      {/* <BlogPage /> */}
+      <CreateBlogPost />
     </Router>
   );
 }
